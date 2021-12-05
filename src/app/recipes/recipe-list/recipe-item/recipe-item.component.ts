f4bde9f02268f497508b35960d881edb78108bf6
Berkay Ulguel <berkayyyulguel@gmail.com>
2021-12-05 18:21:01 +0300

    recipes service developed

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import { RecipesService } from '../../recipes.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,13 +9,14 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-  @Output() recipeSelected = new EventEmitter<Recipe>();
+  // @Output() recipeSelected = new EventEmitter<Recipe>();
 
-  constructor() {}
+  constructor(private recipesService: RecipesService) {}
 
   ngOnInit(): void {}
 
   onSelected() {
-    this.recipeSelected.emit(this.recipe);
+    this.recipesService.recipeSelected.emit(this.recipe);
+    // this.recipeSelected.emit(this.recipe);
   }
 }
